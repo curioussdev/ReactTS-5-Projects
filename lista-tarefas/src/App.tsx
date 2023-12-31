348a043aa11dbd6d9e1c8c39429a10b70f8d7112
José Lopes <946393361bill@gmail.com>
2023-12-31 19:02:54 +0000

    developing the layout

--- lista-tarefas/src/App.tsx
+++ lista-tarefas/src/App.tsx
@@ -1,9 +1,27 @@
+import React, { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/item'; 
 
 const App = () => {
+  const [ list, setList ] = useState<Item[]>([
+    { id: 1, name: '0Comprara Pão', done: false},
+    { id: 2, name: 'Comprar Carro', done: false},
+    
+
+  ]);
+  
   return(
     <C.Container>
-      
+      <C.Area>
+        <C.Header>Lista de Terefas</C.Header>
+
+        {/* Area de adicionar tarefas*/}
+
+        { list.map((item, index) => (
+          <div >{item.name}</div>
+        ))}
+        
+      </C.Area>
     </C.Container>
   );
 };
